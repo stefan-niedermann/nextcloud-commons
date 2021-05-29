7411248e7ff308db3a764335b7359c00d8a7c95b
Stefan Niedermann <info@niedermann.it>
2021-05-29 18:12:29 +0200

    Add some more verbose logging for SingleSignOnUrlLoader#handles

--- sso-glide/src/main/java/it/niedermann/nextcloud/sso/glide/SingleSignOnUrlLoader.kt
+++ sso-glide/src/main/java/it/niedermann/nextcloud/sso/glide/SingleSignOnUrlLoader.kt
@@ -1,6 +1,7 @@
 package it.niedermann.nextcloud.sso.glide
 
 import android.content.Context
+import android.util.Log
 import com.bumptech.glide.load.Options
 import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.load.model.ModelLoader
@@ -16,21 +17,29 @@ import java.io.InputStream
  * A simple model loader for fetching media over http/https using OkHttp.
  */
 class SingleSignOnUrlLoader(private val context: Context) : ModelLoader<GlideUrl, InputStream> {
+    private val TAG = SingleSignOnUrlLoader::class.java.simpleName
+
     override fun handles(url: GlideUrl): Boolean {
+        Log.i(TAG, "[handles] ------------------------------")
         return if (url is SingleSignOnUrl) {
             // It has explicitly been requested, so yeah, we assume that we support this.
+                Log.i(TAG, "[handles] is SingleSignOnUrl → true")
             true
         } else try {
             // We are still not sure whether we can handle this, because it might be from another user account.
             // Though we should try it because it is likely.
 
+            Log.i(TAG, "[handles] starts with / → ${url.toStringUrl()?.startsWith("/")!!}")
+            Log.i(TAG, "[handles] starts with ${SingleAccountHelper.getCurrentSingleSignOnAccount(context).url} → ${url.toStringUrl()?.startsWith(SingleAccountHelper.getCurrentSingleSignOnAccount(context).url)!!}")
             // if it does not start with it but is a relative path, like /foo/bar, we should assume that the resource is on this Nextcloud instance
             url.toStringUrl()?.startsWith("/")!! ||
                     // Or if the url is absolute and points to the url of the currently selected SingleSignOnAccount
                     url.toStringUrl()?.startsWith(SingleAccountHelper.getCurrentSingleSignOnAccount(context).url)!!
         } catch (e: NextcloudFilesAppAccountNotFoundException) {
+            Log.i(TAG, "[handles] NextcloudFilesAppAccountNotFoundException → true")
             false
         } catch (e: NoCurrentAccountSelectedException) {
+            Log.i(TAG, "[handles] NoCurrentAccountSelectedException → true")
             false
         }
     }
