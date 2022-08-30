0f4ef36681abc2e78431093f564128eb613f4f78
Stefan Niedermann <info@niedermann.it>
2022-08-30 15:22:00 +0200

    Add basic unit test

---
+++ markdown/src/test/java/it/niedermann/android/markdown/markwon/processor/PrefixImageDestinationProcessorTest.kt
@@ -0,0 +1,40 @@
+package it.niedermann.android.markdown.markwon.processor
+
+import junit.framework.TestCase
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class PrefixImageDestinationProcessorTest : TestCase() {
+    private lateinit var processor: PrefixImageDestinationProcessor
+
+    @Before
+    fun reset() {
+        processor = PrefixImageDestinationProcessor()
+    }
+
+    @Test
+    fun `should prepend prefix when destination is relative path`() {
+        processor.setPrefix("example.com?url=")
+        assertEquals("example.com?url=bar", processor.process("bar"))
+    }
+
+    @Test
+    fun `should prepend prefix when destination is absolute path`() {
+        processor.setPrefix("example.com?url=")
+        assertEquals("example.com?url=/bar", processor.process("/bar"))
+    }
+
+    @Test
+    fun `should not prepend prefix when destination has valid scheme`() {
+        processor.setPrefix("example.com?url=")
+
+        assertEquals("http://bar", processor.process("http://bar"))
+        assertEquals("https://bar", processor.process("https://bar"))
+        assertEquals("ftp://bar", processor.process("ftp://bar"))
+        assertEquals("ftps://bar", processor.process("ftps://bar"))
+        assertEquals("sftp://bar", processor.process("sftp://bar"))
+    }
+}
